aac534b908abb1f26cec98e07af36ed7152fd558
2111568 <2111568@LTIN448705.cts.com>
2023-07-31 17:07:28 +0530

    Finder Methods

--- productdata/src/main/java/com/nagesh/springdata/product/repos/ProductRepository.java
+++ productdata/src/main/java/com/nagesh/springdata/product/repos/ProductRepository.java
@@ -1,10 +1,22 @@
 package com.nagesh.springdata.product.repos;
-
+ 
 import org.springframework.data.repository.CrudRepository;
 
 import com.nagesh.springdata.product.entities.Product;
+import java.util.List;
 
 public interface ProductRepository extends CrudRepository<Product, Integer> {
 
-	
+	List<Product> findByName(String name);
+
+	List<Product> findByNameAndDescription(String name, String Description);
+
+	List<Product> findByPriceGreaterThan(Double price);
+
+	List<Product> findByDescriptionContains(String description);
+
+	List<Product> findByPriceBetween(Double price1, Double price2);
+
+	List<Product> findByDescriptionLike(String Description);
+
 }
--- productdata/src/test/java/com/nagesh/springdata/ProductdataApplicationTests.java
+++ productdata/src/test/java/com/nagesh/springdata/ProductdataApplicationTests.java
@@ -3,10 +3,11 @@ package com.nagesh.springdata;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
 import com.nagesh.springdata.product.entities.Product;
 import com.nagesh.springdata.product.repos.ProductRepository;
 
@@ -15,7 +16,7 @@ class ProductdataApplicationTests {
 
 	@Autowired
 	ProductRepository repository;
-	
+
 	@Test
 	void contextLoads() {
 	}
@@ -28,39 +29,87 @@ class ProductdataApplicationTests {
 		product.setName("oppo");
 		product.setDescription("osm");
 		product.setPrice(1500d);
-		
+
 		repository.save(product);
 	}
 
 	@Test
 	public void testRead() {
-		
+
 		Product product = repository.findById(2).get();
 		assertNotNull(product);
-		assertEquals("Samsung", product.getName());
+		assertEquals("Tv", product.getName());
 		System.out.println(">>>>>>>>" + product.getId());
-		
+
 	}
+
 	@Test
 	public void testUpdate() {
-		
+
 		Product product = repository.findById(2).get();
-		product.setPrice(2000d);
+		product.setPrice(20000d);
 		repository.save(product);
-		
+
 	}
-	
+
 	@Test
 	public void testDelete() {
-		if(repository.existsById(3)) {
+		if (repository.existsById(3)) {
 			repository.deleteById(3);
-		}	
+		}
 	}
-	
+
 	@Test
 	public void testCount() {
-		
+
 		System.out.println(repository.count());
 	}
 
+	@Test
+	public void testFindByName() {
+
+		List<Product> products = repository.findByName("IWatch");
+		products.forEach(p -> System.out.println(p.getPrice()));
+
+	}
+
+	@Test
+	public void testFindByNameAndDescription() {
+
+		List<Product> products = repository.findByNameAndDescription("Tv", "From Samsung Inc");
+		products.forEach(p -> System.out.println(p.getPrice()));
+
+	}
+
+	@Test
+	public void testFindByPriceGreaterThan() {
+
+		List<Product> products = repository.findByPriceGreaterThan(1000d);
+		products.forEach(p -> System.out.println(p.getPrice()));
+
+	}
+
+	@Test
+	public void testFindByDescriptionContains() {
+
+		List<Product> products = repository.findByDescriptionContains("Apple");
+		products.forEach(p -> System.out.println(p.getPrice()));
+
+	}
+
+	@Test
+	public void testFindByPriceBetween() {
+
+		List<Product> products = repository.findByPriceBetween(400d,3000d);
+		products.forEach(p -> System.out.println(p.getName()));
+	}
+	
+	@Test
+	public void testFindByDescriptionLike() {
+
+		List<Product> products = repository.findByDescriptionLike("%LG%");
+		products.forEach(p -> System.out.println(p.getName()));
+	}
+	
+	
 }
